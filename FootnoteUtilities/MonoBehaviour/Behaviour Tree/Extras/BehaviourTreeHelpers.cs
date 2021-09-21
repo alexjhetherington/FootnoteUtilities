be0b12ef18dcd98f3790452bbb01c49cd0f24854
Alex Hetherington <alexjhetherington@gmail.com>
2021-09-22 00:40:12 +0100

    Behaviour Tree Patrol Subtree

---
+++ FootnoteUtilities/MonoBehaviour/Behaviour Tree/Extras/BehaviourTreeHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BehaviourTreeHelpers
+{
+    public static void AddWaypointsToBlackboard(Brain brain, BehaviourTreeWaypoint[] waypoints, string vector3QueueKey)
+    {
+        Queue<Vector3> queue = new Queue<Vector3>(waypoints.Length);
+
+        foreach (BehaviourTreeWaypoint waypoint in waypoints)
+            queue.Enqueue(waypoint.transform.position);
+
+        brain.Blackboard[vector3QueueKey] = queue;
+    }
+}
--- FootnoteUtilities/MonoBehaviour/Behaviour Tree/Extras/SubTreeLibrary.cs
+++ FootnoteUtilities/MonoBehaviour/Behaviour Tree/Extras/SubTreeLibrary.cs
@@ -1,4 +1,8 @@
-﻿public static class SubTreeLibrary
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class SubTreeLibrary
 {
     public static Node FireBurst(Brain brain, ShootAt shootAt, int burstAmount, float waitBetween)
     {
@@ -8,6 +12,24 @@
             root.Add(shootAt);
             root.Add(new Wait(brain, waitBetween));
         }
+        return root;
+    }
+
+    public static Node Patrol(Brain brain, string vector3QueueKey, string vector3SelectedKey)
+    {
+        Repeater root = new Repeater(brain, new Sequence().Builder()
+                                .Add(new RunAction(() =>
+                                {
+                                    Queue<Vector3> waypoints;
+                                    brain.Blackboard.TryGetTypedValue(vector3QueueKey, out waypoints);
+
+                                    Vector3 next = waypoints.Dequeue();
+                                    waypoints.Enqueue(next);
+                                    brain.Blackboard[vector3SelectedKey] = next;
+                                }))
+                                .Add(new NavTo(brain, vector3SelectedKey, brain.GetComponent<NavMeshAgent>()))
+                            .Build());
+
         return root;
     }
 }
