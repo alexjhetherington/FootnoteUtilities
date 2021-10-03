5f476030ca4bfe8bdfd4edea0b6a715a802c1cf6
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-03 23:03:26 +0100

    WaitUntilTriggered Node

---
+++ FootnoteUtilities/MonoBehaviour/Behaviour Tree/Nodes/General/WaitUntilTriggered.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitUntilTriggered : Node
+{
+    private ParentNode parent;
+
+    public void Cancel()
+    {
+        parent = null;
+    }
+
+    public void Run(ParentNode parent)
+    {
+        this.parent = parent;
+    }
+
+    public void Trigger()
+    {
+        if(parent != null)
+            parent.HandleChildComplete();
+    }
+}
