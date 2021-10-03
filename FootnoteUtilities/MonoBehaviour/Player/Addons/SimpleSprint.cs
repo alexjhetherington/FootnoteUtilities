f4cb1d8f45d5efccbfa71fa75e0268877e9a30fe
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-03 21:29:39 +0100

    Add simple sprint and move folder restructure

---
+++ FootnoteUtilities/MonoBehaviour/Player/Addons/SimpleSprint.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSprint : MonoBehaviour
+{
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float sprintSpeed;
+
+    [SerializeField] private float sprintTime;
+    [SerializeField] private float cooldown;
+    [SerializeField] private float rechargeTime;
+
+    [SerializeField] private KeyCode sprintButton;
+    [SerializeField] private FloatVariable stamina;
+
+    private float cooldownRemaining;
+
+    private Mover mover;
+    private bool isSprinting;
+
+    private void Awake()
+    {
+        mover = GetComponent<Mover>();
+        stamina.Value = 1;
+        mover.SetSpeed(normalSpeed);
+    }
+
+    private void Update()
+    {
+        isSprinting = Input.GetKey(sprintButton);
+
+        if (stamina.Value <= 0)
+        {
+            isSprinting = false;
+            stamina.Value = 0;
+        }
+
+        mover.SetSpeed(isSprinting ? sprintSpeed : normalSpeed);
+
+        if (isSprinting)
+        {
+            cooldownRemaining = cooldown;
+            stamina.Value -= Time.deltaTime / sprintTime;
+        }
+        {
+            cooldownRemaining -= Time.deltaTime;
+
+            if(cooldownRemaining <= 0)
+            {
+                stamina.Value += Time.deltaTime / rechargeTime;
+                stamina.Value = Mathf.Min(1, stamina.Value);
+            }
+        }
+    }
+}
---
+++ FootnoteUtilities/MonoBehaviour/Player/TopDown3dController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDown3dController : MonoBehaviour, Mover
+{
+    [SerializeField] private float speed = 1;
+
+    private CharacterController characterController;
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(horz, 0, vert).normalized;
+        Vector3 velocity = dir * speed;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
