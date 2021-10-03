8d6926e415dfe7d59239488860f48d0949ef8996
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-03 23:52:41 +0100

    Add item added and removed events to RuntimeSet

--- FootnoteUtilities/Scriptable Object Architecture/Objects/RuntimeSet.cs
+++ FootnoteUtilities/Scriptable Object Architecture/Objects/RuntimeSet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,12 +10,51 @@ public abstract class RuntimeSet<T> : ScriptableObject
     public void Add(T thing)
     {
         if (!Items.Contains(thing))
+        {
             Items.Add(thing);
+            for (int i = itemAddedListeners.Count - 1; i >= 0; i--)
+                itemAddedListeners[i].Invoke(thing);
+        }
+            
     }
 
     public void Remove(T thing)
     {
         if (Items.Contains(thing))
+        {
             Items.Remove(thing);
+            for (int i = itemRemovedListeners.Count - 1; i >= 0; i--)
+                itemRemovedListeners[i].Invoke(thing);
+        }   
+    }
+
+    private readonly List<Action<T>> itemAddedListeners =
+           new List<Action<T>>();
+
+    public void RegisterItemAddedListener(Action<T> listener)
+    {
+        if (!itemAddedListeners.Contains(listener))
+            itemAddedListeners.Add(listener);
+    }
+
+    public void UnregisterItemAddedListener(Action<T> listener)
+    {
+        if (itemAddedListeners.Contains(listener))
+            itemAddedListeners.Remove(listener);
+    }
+
+    private readonly List<Action<T>> itemRemovedListeners =
+           new List<Action<T>>();
+
+    public void RegisterItemRemovedListener(Action<T> listener)
+    {
+        if (!itemRemovedListeners.Contains(listener))
+            itemRemovedListeners.Add(listener);
+    }
+
+    public void UnregisterItemRemovedListener(Action<T> listener)
+    {
+        if (itemRemovedListeners.Contains(listener))
+            itemRemovedListeners.Remove(listener);
     }
 }
