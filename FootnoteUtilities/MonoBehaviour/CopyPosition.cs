1218f5ae1270a400fc3d98a970553756de9435b1
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-06 22:18:44 +0100

    Add simple copy position script

---
+++ FootnoteUtilities/MonoBehaviour/CopyPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
+    
+    void LateUpdate()
+    {
+        if(target != null)
+            transform.position = target.position + offset;
+    }
+}
