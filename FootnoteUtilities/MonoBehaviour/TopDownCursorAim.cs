6cfdcc2ac3bcd0d7ac6baadbf948b5e076b7da57
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-06 22:19:36 +0100

    Add simple top down cursor aim script

---
+++ FootnoteUtilities/MonoBehaviour/TopDownCursorAim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCursorAim : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var lookPos = hit.point - transform.position;
+            lookPos.y = 0;
+
+            transform.rotation = Quaternion.LookRotation(lookPos);
+        }
+    }
+}
