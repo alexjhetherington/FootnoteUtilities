ecf66e0ec1adda12ddbe454270e4e44abefafbcc
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-06 22:19:11 +0100

    Add simple OnDiedReplaceWithPrefab script

---
+++ FootnoteUtilities/MonoBehaviour/Health/OnDiedReplaceWithPrefab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDiedReplaceWithPrefab : MonoBehaviour, OnDiedHandler
+{
+    [SerializeField] private GameObject prefab;
+
+    public void OnDied()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
