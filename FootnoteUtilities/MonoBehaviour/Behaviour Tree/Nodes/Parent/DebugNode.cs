94e1c8091f04e1354e0c5a26f9769b3c8047e9e5
Alex Hetherington <alexjhetherington@gmail.com>
2021-10-08 01:55:46 +0100

    Add simple debug and inverter nodes

---
+++ FootnoteUtilities/MonoBehaviour/Behaviour Tree/Nodes/Parent/DebugNode.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugNode : Node, ParentNode
+{
+    private Node child;
+    private ParentNode parent;
+
+    private string name;
+
+    public DebugNode(string name, Node child)
+    {
+        this.name = name;
+        this.child = child;
+    }
+
+    public void Cancel()
+    {
+        child.Cancel();
+    }
+
+    public void Run(ParentNode parent)
+    {
+        Debug.Log(name + " run at " + Time.time);
+        this.parent = parent;
+        child.Run(this);
+    }
+
+    public void HandleChildComplete()
+    {
+        Debug.Log(name + " completed at " + Time.time);
+        parent.HandleChildComplete();
+    }
+
+    public void HandleChildFailed()
+    {
+        Debug.Log(name + " failed at " + Time.time);
+        parent.HandleChildFailed();
+    }
+
+    public void HandleChildInterrupt(Node child)
+    {
+        parent.HandleChildInterrupt(this);
+    }
+
+
+}
---
+++ FootnoteUtilities/MonoBehaviour/Behaviour Tree/Nodes/Parent/Inverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inverter : Node, ParentNode
+{
+    private Node child;
+    private ParentNode parent;
+
+    public Inverter(Node child)
+    {
+        this.child = child;
+    }
+
+    public void Cancel()
+    {
+        child.Cancel();
+    }
+
+    public void Run(ParentNode parent)
+    {
+        this.parent = parent;
+        child.Run(this);
+    }
+
+    public void HandleChildComplete()
+    {
+        parent.HandleChildFailed();
+    }
+
+    public void HandleChildFailed()
+    {
+        parent.HandleChildComplete();
+    }
+
+    public void HandleChildInterrupt(Node child)
+    {
+        parent.HandleChildInterrupt(this);
+    }
+
+    
+}
