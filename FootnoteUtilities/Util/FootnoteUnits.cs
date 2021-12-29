ef70ebeb3c3ac17fdb904b2346c299c2f80e4d95
Alex Hetherington <alexjhetherington@gmail.com>
2021-12-29 19:41:27 +0000

    Add decibel converter

---
+++ FootnoteUtilities/Util/FootnoteUnits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FootnoteUnits
+{
+    private static float linearToDecibels(float linear)
+    {
+        linear = Mathf.Clamp(linear, 0.0001f, 10f);
+        return 20f * Mathf.Log10(linear);
+    }
+    private static float decibelsToLinear(float decibels)
+    {
+        decibels = Mathf.Clamp(decibels, -80f, 20f);
+        return Mathf.Pow(10f, decibels / 20f);
+    }
+}
