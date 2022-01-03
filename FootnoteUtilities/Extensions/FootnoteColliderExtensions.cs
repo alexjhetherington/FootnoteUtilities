c53b69da48e1ac9b364a9c8beea227b1ddfee771
Alex Hetherington <alexjhetherington@gmail.com>
2022-01-03 21:25:46 +0000

    Collider extensions

---
+++ FootnoteUtilities/Extensions/FootnoteColliderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FootnoteColliderExtensions
+{
+    public static Vector3 GetRandomPointInsideCollider(this BoxCollider boxCollider)
+    {
+        Vector3 extents = boxCollider.size / 2f;
+        Vector3 point =
+            new Vector3(
+                Random.Range(-extents.x, extents.x),
+                Random.Range(-extents.y, extents.y),
+                Random.Range(-extents.z, extents.z)
+            ) + boxCollider.center;
+        return boxCollider.transform.TransformPoint(point);
+    }
+}
