57fb15122df2bae5ce4f3c124a406e64fadf909a
Alex Hetherington <alexjhetherington@gmail.com>
2022-01-09 19:47:01 +0000

    Add Default Animation for playable animations

---
+++ FootnoteUtilities/MonoBehaviour/Playable Animation/DefaultAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultAnimation : MonoBehaviour
+{
+    public AnimationClip defaultAnimation;
+
+    [Resolve]
+    public PlayableAnimationController animationController;
+
+    void Start()
+    {
+        animationController.Play(defaultAnimation);
+    }
+}
