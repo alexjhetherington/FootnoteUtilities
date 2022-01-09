23699c118b737893ce32915abe465ce15c700d11
Alex Hetherington <alexjhetherington@gmail.com>
2022-01-09 22:13:48 +0000

    Replace Texture Scroller with Moving Texture

---
+++ FootnoteUtilities/MonoBehaviour/MovingTexture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTexture : MonoBehaviour
+{
+    [SerializeField] private float xScrollSpeed = 0f;
+    [SerializeField] private float yScrollSpeed = 0.1f;
+    private Renderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(xScrollSpeed * Time.time, yScrollSpeed * Time.time));
+    }
+}
