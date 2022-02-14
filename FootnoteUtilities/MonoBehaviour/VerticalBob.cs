c53339a67207aee0f0912e4c480efa71a50f9a00
Alex Hetherington <alexjhetherington@gmail.com>
2022-02-14 01:11:28 +0000

    Add random offset to vertical bob

--- FootnoteUtilities/MonoBehaviour/VerticalBob.cs
+++ FootnoteUtilities/MonoBehaviour/VerticalBob.cs
@@ -2,14 +2,22 @@
 
 public class VerticalBob : MonoBehaviour
 {
-    [SerializeField] private float bobSpeed = 2;
-    [SerializeField] private float bobDistance = 0.2f;
+    public float bobSpeed = 2;
+    public float bobDistance = 0.2f;
+    public bool randomisePhaseOnAwake = true;
 
     private float lastSin = 0f;
+    private float offset = 0;
+
+    void Start()
+    {
+        if (randomisePhaseOnAwake)
+            offset = Random.Range(0, 2 * Mathf.PI);
+    }
 
     void Update()
     {
-        float sin = Mathf.Sin(Time.time * bobSpeed) * bobDistance;
+        float sin = Mathf.Sin(offset + (Time.time * bobSpeed)) * bobDistance;
 
         transform.localPosition += Vector3.up * (sin - lastSin);
 
