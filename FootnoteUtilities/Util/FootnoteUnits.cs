bc3ad70b487e1b712134847320f1cf3dcb6e5ee7
Alex Hetherington <alexjhetherington@gmail.com>
2022-02-19 17:50:33 +0000

    Make decibels easier to work (a slider of 0-10 is sensible, a slider of 0-13 distorts volume)

--- FootnoteUtilities/Util/FootnoteUnits.cs
+++ FootnoteUtilities/Util/FootnoteUnits.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class FootnoteUnits
 {
     public static float linearToDecibels(float linear)
     {
-        linear = Mathf.Clamp(linear, 0.0001f, 10f);
-        return 20f * Mathf.Log10(linear);
+        linear = Mathf.Clamp(linear, 0.0001f, 15f) / 10;
+        return 40f * Mathf.Log10(linear);
     }
     public static float decibelsToLinear(float decibels)
     {
         decibels = Mathf.Clamp(decibels, -80f, 20f);
-        return Mathf.Pow(10f, decibels / 20f);
+        return Mathf.Pow(10f, decibels / 40f) * 10;
     }
 }
