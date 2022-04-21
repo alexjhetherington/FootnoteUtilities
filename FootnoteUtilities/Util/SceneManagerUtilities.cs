6153125d47e255bd46390708a5628abf9b01dbc4
Alex Hetherington <alexjhetherington@gmail.com>
2022-04-21 15:10:27 +0100

    Improve get scene by name logic

--- FootnoteUtilities/Util/SceneManagerUtilities.cs
+++ FootnoteUtilities/Util/SceneManagerUtilities.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class SceneManagerUtilities
 {
@@ -8,7 +11,7 @@ public static class SceneManagerUtilities
         {
             string path = SceneUtility.GetScenePathByBuildIndex(i);
 
-            if (path.Contains("/" + name + ".unity"))
+            if (path.Contains(name))
                 return i;
         }
 
