de238faf1b25f81185882a235cdadc57b780e545
Alex Hetherington <alexjhetherington@gmail.com>
2022-04-21 15:09:13 +0100

    Add On Mouse Over ui extension

---
+++ FootnoteUtilities/Ui/OnMouseOver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Action OnEnter;
+    public Action OnExit;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnEnter.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnExit.Invoke();
+    }
+}
