658c7911991395c5749957584425f06ca8a19104
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:33:23 +0000

    On Trigger exit / enter sound fade

---
+++ FootnoteUtilities/MonoBehaviour/EnterTriggerSoundFade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTriggerSoundFade : MonoBehaviour
+{
+    public Tag triggerByTag;
+    public bool compound = false;
+    public AudioSource audioSource;
+
+    public float targetVolume;
+
+    public float fadeSpeed;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (compound)
+            return;
+
+        if (other.HasTag(triggerByTag))
+        {
+            SoundManager.FadeTo(audioSource, targetVolume, fadeSpeed);
+        }
+    }
+
+    public void OnCompoundTriggerEnter(Collider other)
+    {
+        if (!compound)
+            return;
+
+        if (other.HasTag(triggerByTag))
+        {
+            SoundManager.FadeTo(audioSource, targetVolume, fadeSpeed);
+        }
+    }
+}
---
+++ FootnoteUtilities/MonoBehaviour/ExitTriggerSoundFade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTriggerSoundFade : MonoBehaviour
+{
+    public Tag triggerByTag;
+    public bool compound = false;
+    public AudioSource audioSource;
+
+    public float targetVolume;
+
+    public float fadeSpeed;
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (compound)
+            return;
+
+        if (other.HasTag(triggerByTag))
+        {
+            SoundManager.FadeTo(audioSource, targetVolume, fadeSpeed);
+        }
+    }
+
+    public void OnCompoundTriggerExit(Collider other)
+    {
+        if (!compound)
+            return;
+
+        if (other.HasTag(triggerByTag))
+        {
+            SoundManager.FadeTo(audioSource, targetVolume, fadeSpeed);
+        }
+    }
+}
