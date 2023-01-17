bd8e7cafefd0c8ca2c8582d7e20ff67c4636c5f1
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:32:10 +0000

    URL Button

---
+++ FootnoteUtilities/MonoBehaviour/URLButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLButton : MonoBehaviour
+{
+    public string url;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => Application.OpenURL(url));
+    }
+}
