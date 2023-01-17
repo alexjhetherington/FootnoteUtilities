395e20ecaef177fb38c8390007c2bb18de599e3e
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:32:18 +0000

    Simple culling group

---
+++ FootnoteUtilities/MonoBehaviour/SimpleCullingGroup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Recommend to be used with OnTriggerTagEnter and OnTriggerTagExit
+public class SimpleCullingGroup : MonoBehaviour
+{
+    public GameObject[] contents;
+    private Coroutine coroutine;
+    private int defaultFrameDelay = 2;
+
+    int viewers = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cull(true, 1);
+    }
+
+    public void AddViewer()
+    {
+        AddViewer(defaultFrameDelay);
+    }
+
+    public void RemoveViewer()
+    {
+        RemoveViewer(defaultFrameDelay);
+    }
+
+    public void AddViewer(int frameDelay)
+    {
+        if (viewers++ == 0)
+            Cull(false, frameDelay);
+    }
+
+    public void RemoveViewer(int frameDelay)
+    {
+        if (--viewers == 0)
+            Cull(true, frameDelay);
+    }
+
+    private void Cull(bool value, int frameDelay)
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+
+        coroutine = StartCoroutine(CullOverFrames(value, frameDelay));
+    }
+
+    IEnumerator CullOverFrames(bool value, int frameDelay)
+    {
+        foreach (GameObject go in contents)
+        {
+            for (int i = 0; i < frameDelay; i++)
+            {
+                yield return null;
+            }
+
+            go.SetActive(!value);
+        }
+    }
+}
