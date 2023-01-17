fc2c40c9171ddf153a1d57b189dbc6fc2ec7c25f
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:29:38 +0000

    Get First Active (run time set)

--- FootnoteUtilities/Scriptable Object Architecture/Objects/MonoBehaviourRuntimeSet.cs
+++ FootnoteUtilities/Scriptable Object Architecture/Objects/MonoBehaviourRuntimeSet.cs
@@ -6,15 +6,63 @@ using UnityEngine;
 [CreateAssetMenu]
 public class MonoBehaviourRuntimeSet : RuntimeSet<MonoBehaviour>
 {
-    //TODO bad allocation
+    //TODO bad allocation?
     public IEnumerable<T> GetAs<T>() where T : MonoBehaviour
     {
         foreach (MonoBehaviour monoBehaviour in Items)
         {
             if (monoBehaviour is T)
+            {
                 yield return (T)monoBehaviour;
+            }
             else
-                yield return monoBehaviour.GetComponent<T>();
+            {
+                T component = monoBehaviour.GetComponentInChildren<T>();
+                if (component != null)
+                {
+                    yield return component;
+                }
+                else
+                {
+                    Debug.LogError(
+                        "Looks like you incorrectly set group ["
+                            + this.name
+                            + "] on object ["
+                            + monoBehaviour.name
+                            + "]"
+                    );
+                    continue;
+                }
+            }
         }
+    }
+
+    public T GetFirstActiveAs<T>() where T : MonoBehaviour
+    {
+        foreach (MonoBehaviour monoBehaviour in Items)
+        {
+            if (monoBehaviour.gameObject.activeInHierarchy && monoBehaviour.isActiveAndEnabled)
+            {
+                if (monoBehaviour is T)
+                    return (T)monoBehaviour;
+
+                T component = monoBehaviour.GetComponentInChildren<T>();
+                if (component != null)
+                {
+                    return component;
+                }
+                Debug.LogError(
+                    "Looks like you incorrectly set group ["
+                        + this.name
+                        + "] on object ["
+                        + monoBehaviour.name
+                        + "]"
+                );
+                continue;
+            }
+        }
+        throw new Exception(
+            "No live gameobjects of type [" + typeof(T) + "] in runtime set [" + name + "]"
+        );
     }
 }
