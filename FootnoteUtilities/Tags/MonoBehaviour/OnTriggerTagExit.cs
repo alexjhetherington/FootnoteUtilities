51c2f5f78265b9c8b46af7b359b353073f584418
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:32:02 +0000

    On Trigger Tag Exit

---
+++ FootnoteUtilities/Tags/MonoBehaviour/OnTriggerTagExit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerTagExit : MonoBehaviour
+{
+    public Tag checkTag;
+    public bool compound = false;
+    public UnityEvent unityEvent;
+
+    void OnTriggerExit(Collider other)
+    {
+        if (compound)
+            return;
+
+        if (other.HasTag(checkTag))
+        {
+            unityEvent.Invoke();
+        }
+    }
+
+    void OnCompoundTriggerExit(Collider other)
+    {
+        if (!compound)
+            return;
+
+        if (other.HasTag(checkTag))
+        {
+            unityEvent.Invoke();
+        }
+    }
+}
