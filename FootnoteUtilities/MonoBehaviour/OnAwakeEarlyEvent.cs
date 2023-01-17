98fc31104b53ed48cbecc7c22c89bb81bf7b2bdd
Alex Hetherington <alexjhetherington@gmail.com>
2023-01-17 17:34:26 +0000

    Some simple awake / start events

---
+++ FootnoteUtilities/MonoBehaviour/OnAwakeEarlyEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[DefaultExecutionOrder(-100)]
+public class OnAwakeEarlyEvent : MonoBehaviour
+{
+    public UnityEvent unityEvent;
+
+    void Awake()
+    {
+        unityEvent.Invoke();
+    }
+}
---
+++ FootnoteUtilities/MonoBehaviour/OnStartNextFrameEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnStartNextFrameEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent unityEvent;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        unityEvent.Invoke();
+    }
+}
